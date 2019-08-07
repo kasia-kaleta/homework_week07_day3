6e623fe5b28b34595062023f848b5e57507fd84b
kat3005 <kasia.alicja@gmail.com>
2019-08-07 20:00:15 +0100

    Adds changes

---
+++ src/components/CountriesList.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div>
+    <ul>
+      <list-item v-for="(country, index) in countries" :country="country" :key="index"></list-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+import ListComponent from './ListComponent.vue';
+
+export default {
+  name: 'countries-list',
+  props: ['countries'],
+  components: {
+    "list-item": ListComponent
+  }
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
